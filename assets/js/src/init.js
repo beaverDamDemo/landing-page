4f8240cf3c90edd74fd849d140a1acfe6a714625
zdenevuk <zdenko.vuk@.ijs.si>
2023-05-15 21:39:18 +0200

    on large screens now works fine

--- assets/js/src/init.js
+++ assets/js/src/init.js
@@ -1,7 +1,7 @@
 (function () {
-  'use strict';
-  var currentStep = 0
-  $('body').removeClass('loading')
+  "use strict";
+  var currentStep = 0;
+  $("body").removeClass("loading");
 
   /* preloader */
   var now = new Date().getTime();
@@ -10,85 +10,88 @@
   var width = 100,
     perfData = window.performance.timing,
     EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
-    time = parseInt((EstimatedTime/1000)%60)*100;
-    console.log("estimated time: ", time)
-  $('.loadbar').animate({
-    'width': width+'%'
-  }, time)
+    time = parseInt((EstimatedTime / 1000) % 60) * 100;
+  console.log("estimated time: ", time);
+  $(".loadbar").animate(
+    {
+      width: width + "%",
+    },
+    time
+  );
 
   function preloaderTimeout() {
     var dfd = $.Deferred();
-    console.warn("temporary")
-    setTimeout(()=>{
-      dfd.resolve('preloader timeout passed')
-    }, time)
+    console.warn("temporary");
+    setTimeout(() => {
+      dfd.resolve("preloader timeout passed");
+    }, time);
     return dfd.promise();
   }
 
   /* load json with skills */
-  $.getJSON("assets/json/data.json", function(json) {
-      console.log(json); // this will show the info it in firebug console
-      $('.preloader-wrapper').removeClass('active');
+  $.getJSON("assets/json/data.json", function (json) {
+    console.log(json); // this will show the info it in firebug console
+    $(".preloader-wrapper").removeClass("active");
 
-      for( var i=0; i<json.skills.length; i++ ) {
-        var appendString = '';
-        appendString += "<div class='skill-wrapper'>"
-        appendString += "<div class='skill skill__label'>"+json.skills[i].label+"</div>"
-        appendString += "<div class='skill skill__value'>"+json.skills[i].value+"</div>"
-        appendString += "<div class='skill skill__bar' style='max-width: "+(42*json.skills[i].value)+"%'>"
-        appendString += "<div class='inner'></div></div>"
-        appendString += "</div>"
-        $('section#section-skills .wrapper').append(appendString)
-      }
+    for (var i = 0; i < json.skills.length; i++) {
+      var appendString = "";
+      appendString += "<div class='skill-wrapper'>";
+      appendString +=
+        "<div class='skill skill__label'>" + json.skills[i].label + "</div>";
+      appendString +=
+        "<div class='skill skill__value'>" + json.skills[i].value + "</div>";
+      appendString += "<div class='skill skill__bar'>";
+      appendString += "<div class='skill__bar__background'>";
+      appendString +=
+        "<div class='skill__bar__fixed' >";
+        appendString +=
+        "<div class='skill__bar__dynamic' style='max-width: " +
+        (100 * (1 - json.skills[i].value)) +
+        "%'></div></div></div>";
+      appendString += "</div></div>";
+      $("section#section-skills .wrapper").append(appendString);
+    }
   });
 
-  $('#header__icon').click(function(e){
+  $("#header__icon").click(function (e) {
     e.preventDefault();
-    $('body').toggleClass('with--sidebar');
+    $("body").toggleClass("with--sidebar");
   });
 
-  $('#site-cache').click(function(e){
-    $('body').removeClass('with--sidebar');
+  $("#site-cache").click(function (e) {
+    $("body").removeClass("with--sidebar");
   });
 
-  $('header a').on('click', function(){
-     $('header a').removeClass('active')
-     $(this).addClass('active')
-  })
+  $("header a").on("click", function () {
+    $("header a").removeClass("active");
+    $(this).addClass("active");
+  });
 
+  $("main").addClass("active");
+  $(".navigation-wrapper").addClass("active");
 
-  $('main').addClass('active');
-  $('.navigation-wrapper').addClass('active');
-
-  $('#playButton').one('click', function(e) {
-    $('#playButton').addClass('hidden');
-    if(createjs.WebAudioPlugin.isSupported()) {
+  $("#playButton").one("click", function (e) {
+    $("#playButton").addClass("hidden");
+    if (createjs.WebAudioPlugin.isSupported()) {
       createjs.WebAudioPlugin.context.resume();
     }
   });
 
-  $("nav a").on('click', function(e) {
-    var diff = Math.abs(currentStep - parseInt($(this).attr('id').split('-')[2]))
-    currentStep = parseInt($(this).attr('id').split('-')[2])
-    $('main').css({
-      'transform': 'translateX(-'+(parseInt($(this).attr('id').split('-')[2])*20)+'%)',
-      'transition': 'transform '+diff*0.8+'s'
-    })
-  })
+  $("nav a").on("click", function (e) {
+    var diff = Math.abs(
+      currentStep - parseInt($(this).attr("id").split("-")[2])
+    );
+    currentStep = parseInt($(this).attr("id").split("-")[2]);
+    $("main").css({
+      transform:
+        "translateX(-" + parseInt($(this).attr("id").split("-")[2]) * 20 + "%)",
+      transition: "transform " + diff * 0.8 + "s",
+    });
+  });
 
-  $('#a-link-0').on('click', function() {
-
-  })
-  $('#a-link-1').on('click', function() {
-
-  })
-  $('#a-link-2').on('click', function() {
-
-  })
-  $('#a-link-3').on('click', function() {
-
-  })
-  $('#a-link-4').on('click', function() {
-
-  })
+  $("#a-link-0").on("click", function () {});
+  $("#a-link-1").on("click", function () {});
+  $("#a-link-2").on("click", function () {});
+  $("#a-link-3").on("click", function () {});
+  $("#a-link-4").on("click", function () {});
 })();
